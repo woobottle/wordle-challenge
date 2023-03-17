af3dd47c29609f23c9e80c77a8b7fff627d29755
wooBottle <dkrnfls@gmail.com>
2023-03-17 12:09:20 +0900

    reset temporary work

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { Header, Wordle } from "./components";
 function App() {
   return (
     <>
-      <h1>금요일 19시 30분 평촌 무리소</h1>
-      <h2>참가자 : 우병, 연호, 윤상, 수영, 재권</h2>
-      {/* <Header />
-      <Wordle /> */}
+      <Header />
+      <Wordle />
     </>
   );
 }
